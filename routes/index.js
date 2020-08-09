d386606709f99ed157cff4754958de176306c699
ginid <ginidthakur@mansainfotech.com>
2020-08-09 16:53:23 +0530

    test

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,7 @@ var router = express.Router();
 var userRoutes = require("../api/user/user");
 
 router.use("/user", userRoutes);
-
+router.use("/test", (req, res, next) => {
+  res.send("req test");
+});
 module.exports = router;
